2a4ec702f94580a0162ddaeb3506920586b8f0bc
syakoo <syakoo.pub@gmail.com>
2022-08-03 20:20:19 +0900

    🔧 chore: initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+// @ts-check
+const { defineConfig } = require("eslint-define-config");
+
+module.exports = defineConfig({
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ["tsconfig.json", "packages/todo-mock-api/tsconfig.json"],
+      },
+    },
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-var-requires": "off",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "index",
+          "sibling",
+          "type",
+          "object",
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {},
+    },
+  ],
+});
---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+// @ts-check
+import path from "path";
+
+import typescript from "@rollup/plugin-typescript";
+
+import pkg from "./package.json";
+
+const banner = `/*!
+  ${pkg.name} v${pkg.version}
+  ${pkg.homepage}
+  Released under the ${pkg.license} License.
+*/`;
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config = {
+  input: path.resolve(__dirname, "src/index.ts"),
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: "inline",
+      banner,
+    },
+  ],
+  plugins: [
+    typescript({
+      tsconfig: path.resolve(__dirname, "./tsconfig.json"),
+    }),
+  ],
+};
+
+export default [config];
