98e608c23e57d457f5fb7f1cdedd1717e2c3bbe9
syakoo <syakoo.pub@gmail.com>
2022-08-06 12:23:32 +0900

    🔧 chore: add cac and eslint-import-resolver-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { defineConfig } = require("eslint-define-config");
+const { defineConfig } = require('eslint-define-config');
 
 module.exports = defineConfig({
   env: {
@@ -7,52 +7,44 @@ module.exports = defineConfig({
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        alwaysTryTypes: true,
-        project: ["tsconfig.json", "packages/todo-mock-api/tsconfig.json"],
-      },
-    },
-  },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    "@typescript-eslint/no-var-requires": "off",
-    "import/order": [
-      "error",
+    'import/order': [
+      'error',
       {
         groups: [
-          "builtin",
-          "external",
-          "internal",
-          "parent",
-          "index",
-          "sibling",
-          "type",
-          "object",
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'index',
+          'sibling',
+          'type',
+          'object',
         ],
-        "newlines-between": "always",
         alphabetize: {
-          order: "asc",
+          order: 'asc',
         },
       },
     ],
   },
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"],
-      rules: {},
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ['./tsconfig.json'],
+      },
     },
-  ],
+  },
 });
