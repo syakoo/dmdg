4e6156cf780030a421a6d39bac35692de58ef109
syakoo <syakoo.pub@gmail.com>
2022-08-19 22:56:26 +0900

    ✨ feat: set baseDir to current dir.

--- src/drawer.ts
+++ src/drawer.ts
@@ -22,7 +22,7 @@ export function drawDependencyGraph(entryDir: string, option: CruiseOption) {
       },
       collapse: '^node_modules/[^/]+',
       focus: entryDir.replace('./', ''),
-      baseDir: '.',
+      baseDir: process.cwd(),
       ruleSet: {
         // Without this setting, path aliases will not be resolved.
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
